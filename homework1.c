b5cb9628bbf7a3f1a5d391a2bb6235b33d5d7380
pilpin79 <2003david8@gmail.com>
2022-11-11 10:19:12 +0200

    hello world!

---
+++ homework1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int sum_1, sum_2;
+    scanf("%d", &sum_1);
+    scanf("%d", &sum_2);
+    printf ("Hello World! number is: %d", sum_1 + sum_2);
+    return 0;
+}
